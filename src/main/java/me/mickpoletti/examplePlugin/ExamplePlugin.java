6f82d459a72c9a7c0d9429c188a0909baf19d54c
Mason McDaniel <mason.mcdaniel.github@gmail.com>
2025-04-04 22:55:08 -0500

    Add permissions example

--- src/main/java/me/mickpoletti/examplePlugin/ExamplePlugin.java
+++ src/main/java/me/mickpoletti/examplePlugin/ExamplePlugin.java
@@ -2,6 +2,7 @@ package me.mickpoletti.examplePlugin;
 
 import me.mickpoletti.examplePlugin.commands.FeedCommand;
 import me.mickpoletti.examplePlugin.commands.GodCommand;
+import me.mickpoletti.examplePlugin.commands.PogCommand;
 import me.mickpoletti.examplePlugin.listeners.DeathListener;
 import me.mickpoletti.examplePlugin.listeners.JoinLeaveListener;
 import me.mickpoletti.examplePlugin.listeners.ShearSheepListener;
@@ -10,7 +11,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 // This is a first example of how to use a listener, however they should be
@@ -34,6 +34,7 @@ public final class ExamplePlugin extends JavaPlugin implements Listener {
         getCommand("god").setExecutor(new GodCommand());
         // Register feed command
         getCommand("eat").setExecutor(new FeedCommand());
+        getCommand("pog").setExecutor(new PogCommand());
     }
     @Override
     public void onDisable() {
---
+++ src/main/java/me/mickpoletti/examplePlugin/commands/PogCommand.java
@@ -0,0 +1,25 @@
+package me.mickpoletti.examplePlugin.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class PogCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command c, String label, String[] args ) {
+        if (sender instanceof Player) {
+            Player p = ((Player) sender).getPlayer();
+            if (p.hasPermission("exampleplugin.pog")) {
+                p.sendMessage("You are indeed "
+                        + ChatColor.BOLD + ChatColor.DARK_PURPLE + "POG");
+            } else {
+                p.sendMessage("YOU ARE "
+                        + ChatColor.BOLD + ChatColor.RED + ChatColor.UNDERLINE + "NOT"
+                        + ChatColor.RESET + " POG!!!!!!! ☹");
+            }
+        }
+        return true;
+    }
+}
