9f5ca7c058c6ddc0a66426471476d59468141803
Mason McDaniel <mason.mcdaniel.github@gmail.com>
2025-04-26 16:58:14 -0500

    Add an example of a command timeout to the PogCommand This implementation uses a hash map to store the user's UUID and the epoch time of when they last ran the command I added this example to the PogCommand because I didn't really want to create the tutorial's example, FartCommand, but also because I felt that it felt the spirit of the PogCommand. Perhaps I'll come back and edit the player messages later to be more creative and in-theme.

--- src/main/java/me/mickpoletti/examplePlugin/commands/PogCommand.java
+++ src/main/java/me/mickpoletti/examplePlugin/commands/PogCommand.java
@@ -6,20 +6,44 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class PogCommand implements CommandExecutor {
+    // uuid = is the userid of the player
+    // Long = the epoch time when the player issued the command
+    private final HashMap<UUID, Long> cooldown;
+
+    public PogCommand() {
+        this.cooldown = new HashMap<>();
+    }
     @Override
     public boolean onCommand(CommandSender sender, Command c, String label, String[] args ) {
         if (sender instanceof Player) {
             Player p = ((Player) sender).getPlayer();
-            if (p.hasPermission("exampleplugin.pog")) {
-                p.sendMessage("You are indeed "
-                        + ChatColor.BOLD + ChatColor.DARK_PURPLE + "POG");
+            UUID uuid = p.getUniqueId();
+            long time_elapsed = System.currentTimeMillis() - cooldown.get(uuid);
+            long TIMEOUT = 10000;
+            if (!this.cooldown.containsKey(uuid) || time_elapsed > TIMEOUT) {
+                PogCommandMessage(p);
+                this.cooldown.put(uuid, System.currentTimeMillis());
             } else {
-                p.sendMessage("YOU ARE "
-                        + ChatColor.BOLD + ChatColor.DARK_RED + ChatColor.UNDERLINE + "NOT"
-                        + ChatColor.RESET + " POG!!!!!!! ☹");
+                p.sendMessage(ChatColor.RED + "ERROR: " + ChatColor.RESET +
+                            "You cannot use this command for another "
+                            + (TIMEOUT - time_elapsed) + " milliseconds.");
             }
         }
         return true;
+    }
+
+    public final void PogCommandMessage(Player p) {
+        if (p.hasPermission("exampleplugin.pog")) {
+            p.sendMessage("You are indeed "
+                    + ChatColor.BOLD + ChatColor.DARK_PURPLE + "POG");
+        } else {
+            p.sendMessage("YOU ARE "
+                    + ChatColor.BOLD + ChatColor.DARK_RED + ChatColor.UNDERLINE + "NOT"
+                    + ChatColor.RESET + " POG!!!!!!! ☹");
+        }
     }
 }
